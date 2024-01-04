9c2e00e86bb66586e4b9e5f6604024f5c90c745d
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2024-01-05 00:25:20 +0500

    form created and ready to send data to backend

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../shared/interfaces";
+import {AuthService} from "../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {
+  }
+
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
+    // console.log(this.form)
+  }
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    const user: User = {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    }
+
+    this.auth.login(user).subscribe(() => {
+      this.form.reset()
+      this.router.navigate(['/admin', 'dashboard'])
+    })
+  }
 
 }
