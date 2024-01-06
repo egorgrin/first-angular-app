44f082931d72a9779f1907fe8986153f42ef38f1
Egor Grin <35704842+egorgrin@users.noreply.github.com>
2024-01-06 17:30:56 +0500

    Main page post logic added

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostService} from "../shared/post.service";
+import {ActivatedRoute, Params} from "@angular/router";
+import {Observable, switchMap} from "rxjs";
+import {Post} from "../shared/interfaces";
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss']
 })
-export class PostPageComponent {
+export class PostPageComponent implements OnInit {
+
+  post$: Observable<Post>
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService
+  ) {
+  }
+
+  ngOnInit() {
+    this.post$ = this.route.params
+      .pipe(switchMap((params: Params) => {
+        return this.postService.getById(params['id'])
+      }))
+
+  }
 
 }
